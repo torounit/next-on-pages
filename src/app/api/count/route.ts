0f7879a777b0c413863df32d98aa79940877f084
Hiroshi Urabe <mail@torounit.com>
2024-09-08 14:50:47 +0900

    add response header

--- src/app/api/count/route.ts
+++ src/app/api/count/route.ts
@@ -9,5 +9,8 @@ export async function GET(request: NextRequest) {
 	const myKv = getRequestContext().env.NEXT_ON_PAGES
 	const count = toInt(await myKv.get('count'));
 	await myKv.put('count', String(count + 1));
-	return new Response(`Count: ${count + 1}`);
+	await new Promise(resolve => setTimeout(resolve, 1000))
+	const response = new Response(`Count: ${count + 1}`);
+	response.headers.set('Cache-Control', 'max-age=10, stale-while-revalidate=60')
+	return response;
 }
