9722c48d1fec5d72beb816928a46a951777c4865
Hiroshi Urabe <mail@torounit.com>
2024-09-08 15:14:53 +0900

    remove header

--- src/app/api/count/route.ts
+++ src/app/api/count/route.ts
@@ -9,8 +9,7 @@ export async function GET(request: NextRequest) {
 	const myKv = getRequestContext().env.NEXT_ON_PAGES
 	const count = toInt(await myKv.get('count'));
 	await myKv.put('count', String(count + 1));
-	await new Promise(resolve => setTimeout(resolve, 1000))
+	await new Promise(resolve => setTimeout(resolve, 3000))
 	const response = new Response(`Count: ${count + 1}`);
-	response.headers.set('Cache-Control', 'max-age=10, stale-while-revalidate=60')
 	return response;
 }
